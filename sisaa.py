e19b797ae77049deb09f75025a3b36cbc18ef379
Ciro Morais <ciromoraismedeiros@gmail.com>
2013-11-27 10:43:27 -0300

    Fazendo o cadastro de GT

--- sisaa.py
+++ sisaa.py
@@ -2,6 +2,7 @@ from google.appengine.ext import webapp
 
 from handlers import *
 
+
 app = webapp.WSGIApplication([('/', InicioHandler),
                               ('/cad_gt', CadGTHandler),],
                               debug=True)
